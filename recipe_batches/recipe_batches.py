6a80cbaf97fd6623360e481211ccb4a1b2cb10d5
ken1286 <kindle1286@gmail.com>
2019-10-09 14:59:18 -0700

    finished recipes

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,29 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    # check how many can be made from each ingredient
+    # get lowest amount from all possibilities
+    # also check if recipe key exists in ingredients.keys()
+
+    possible_batches = []
+    for rk, rv in recipe.items():
+        for ik, iv in ingredients.items():
+            if rk == ik:
+                amount = iv // rv
+                # print(amount)
+                possible_batches.append(amount)
+        if rk not in ingredients.keys():
+            possible_batches.append(0)
+
+    return min(possible_batches)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
